96696a0f0dcd737350f4a5b05a99bc7749930f71
SB <sb@SBs-MacBook-Pro.local>
2019-01-24 14:12:21 -0500

    Set up storyboard with an image view, some labels, a few text fields, a button and a picker view, also added relevant IBOutlet vars and IBAction func to ViewController

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var statePicker: UIPickerView!
+    @IBOutlet var statePickerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateButtonPressed(_ sender: UIButton) {
+        
+    }
 
 }
 
