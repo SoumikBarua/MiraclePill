0a2c89ff741fe5da942d63075ad6590e1b6f4bad
SB <sb@SBs-MacBook-Pro.local>
2019-01-24 15:26:02 -0500

    Updated the width of picker view, made ViewController comform to UIPickerViewDelegate and UIPickerViewDataSource and implemented the necessary functions, added string array of 50 states and configured the button for picker view selection and hiding

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,14 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet var statePicker: UIPickerView!
     @IBOutlet var statePickerButton: UIButton!
     
+    let states = ["Alabama",
+                  "Alaska",
+                  "Arizona",
+                  "Arkansas",
+                  "California",
+                  "Colorado",
+                  "Connecticut",
+                  "Delaware",
+                  "Florida",
+                  "Georgia",
+                  "Hawaii",
+                  "Idaho",
+                  "Illinois",
+                  "Indiana",
+                  "Iowa",
+                  "Kansas",
+                  "Kentucky",
+                  "Louisiana",
+                  "Maine",
+                  "Maryland",
+                  "Massachusetts",
+                  "Michigan",
+                  "Minnesota",
+                  "Mississippi",
+                  "Missouri",
+                  "Montana",
+                  "Nebraska",
+                  "Nevada",
+                  "New Hampshire",
+                  "New Jersey",
+                  "New Mexico",
+                  "New York",
+                  "North Carolina",
+                  "North Dakota",
+                  "Ohio",
+                  "Oklahoma",
+                  "Oregon",
+                  "Pennsylvania",
+                  "Rhode Island",
+                  "South Carolina",
+                  "South Dakota",
+                  "Tennessee",
+                  "Texas",
+                  "Utah",
+                  "Vermont",
+                  "Virginia",
+                  "Washington",
+                  "West Virginia",
+                  "Wisconsin",
+                  "Wyoming"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        statePicker.delegate = self
+        statePicker.dataSource = self
+        
+        statePicker.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +80,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func stateButtonPressed(_ sender: UIButton) {
-        
+        statePicker.isHidden = false
+    }
+    
+    
+    // MARK: - UIPickerViewDataSource methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    
+    // MARK: - UIPickerViewDelgate methods
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: .normal)
+        statePicker.isHidden = true
     }
 
 }
